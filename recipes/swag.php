918f867cf7883af86d483405add84cebbd176131
Feodor Rusanov <f.rusanov@gmail.com>
2021-06-05 02:17:31 +0300

    Fix run function argument

--- recipes/swag.php
+++ recipes/swag.php
@@ -3,26 +3,26 @@
 namespace Deployer;
 
 task('swag:create', function () {
-  run(`docker create \
-		--name=swag \
-		--cap-add=NET_ADMIN \
-		-e PUID=$PUID \
-		-e PGID=$GUID \
-		-e TZ=$TZ \
-		-e URL=$URL \
-		-e SUBDOMAINS=www, \
-		-e VALIDATION=http \
-		-e EMAIL=$EMAIL  \
-		-e ONLY_SUBDOMAINS=false  \
-		-e EXTRA_DOMAINS=$EXTRA_DOMAINS  \
-		-e STAGING=false  \
-		-p 443:443 \
-		-p 80:80  \
-		-v ./.config/swag:/config \
-		--restart unless-stopped \
-		ghcr.io/linuxserver/swag`);
+  run("docker create"
+    . " --name=swag"
+    . " --cap-add=NET_ADMIN"
+    . " -e PUID=$PUID"
+    . " -e PGID=$GUID"
+    . " -e TZ=$TZ"
+    . " -e URL=$URL"
+    . " -e SUBDOMAINS=www,"
+    . " -e VALIDATION=http"
+    . " -e EMAIL=$EMAIL"
+    . " -e ONLY_SUBDOMAINS=false"
+    . " -e EXTRA_DOMAINS=$EXTRA_DOMAINS"
+    . " -e STAGING=false"
+    . " -p 443:443"
+    . " -p 80:80"
+    . " -v ./.config/swag:/config"
+    . " --restart unless-stopped"
+    . " ghcr.io/linuxserver/swag");
 });
 
 task('swag:remove', function () {
-  run(`docker container rm swag`);
+  run("docker container rm swag");
 });
