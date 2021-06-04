d45c5427e679d3cb42b847b166736c284e783a2b
Feodor Rusanov <f.rusanov@gmail.com>
2021-06-05 02:01:13 +0300

    Fix dotenv loading

--- deploy.php
+++ deploy.php
@@ -4,6 +4,10 @@ namespace Deployer;
 
 require 'recipe/common.php';
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/deployer/load-env.php';
 
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->required(['URL']);
+$dotenv->load();
+
+require __DIR__ . '/deployer/env.php';
 require __DIR__ . '/recipes/swag.php';
--- deployer/env.php
+++ deployer/env.php
@@ -1,7 +1,4 @@
 <?php
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->required(['URL']);
-$dotenv->load();
 
 $PUID = getSafeEnv('PUID', 1000);
 $PGID = getSafeEnv('PGID', 1000);
