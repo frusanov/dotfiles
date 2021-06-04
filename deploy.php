ffc87180ed60624cd1c216b25552443e0adf3f81
Feodor Rusanov <f.rusanov@gmail.com>
2021-06-05 02:09:45 +0300

    Fix dotenv calls order

--- deploy.php
+++ deploy.php
@@ -6,8 +6,8 @@ require 'recipe/common.php';
 require __DIR__ . '/vendor/autoload.php';
 
 $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->required(['URL']);
 $dotenv->load();
+$dotenv->required(['URL']);
 
 require __DIR__ . '/deployer/env.php';
 require __DIR__ . '/recipes/swag.php';
